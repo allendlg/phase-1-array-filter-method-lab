30bcfe58fb2638e21a8bee354575e971dd3aa23c
allendlg <allen.delagarza01@gmail.com>
2021-07-04 01:33:37 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => driver.substring(0, name.length) === name)
+}
+
+function matchName(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.name === string;
+    })
+}
